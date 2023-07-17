d57313140e6964a2cc937b7eefef2f5111c7b8a5
farshidDev <ahmadi.farshid108@gmail.com>
2023-07-17 04:51:14 +0430

    visibility and invisibility of modal and handling memory leak for modal service

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@angular/core';
 
+interface IModal{
+  id: string;
+  visible: boolean
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  visible = false;
+  private modals: IModal[] = [];
 
   constructor() { }
+
+  isModalVisible(id: string): boolean
+  {
+    return Boolean(this.modals.find(element => element.id === id)?.visible);
+  }
+
+  toggleModal(id: string)
+  {
+    const modal = this.modals.find(element => element.id === id);
+    if(modal)
+    {
+      modal.visible = !modal.visible;
+    }
+  }
+
+  register(id:string)
+  {
+    this.modals.push(
+      {
+        id,
+        visible: false
+      }
+    )
+
+  }
+
+  unregister(id:string)
+  {
+    this.modals = this.modals.filter(
+      element => element.id !== id
+    );
+  }
+
 }
